bdfbfa5e84f53915db08372fd2cecf51be4b024e
Igor Rzegocki <igor@rzegocki.pl>
2024-03-15 22:21:06 +0100

    chore(deps): update schemas

--- build.sh
+++ build.sh
@@ -2,10 +2,7 @@
 
 set -e
 
-git checkout gh-pages
 [ -f "${KUBECONFIG}" ] || (echo "Missing kubeconfig" && exit 1)
 docker run --rm -it -v "${KUBECONFIG}:/root/.kube/config" -v "$(pwd):/crds" -e PUID=1000 -e PGID=1000 ghcr.io/deedee-ops/crd-extractor
 git add -A
-git commit -m "$(date)"
-git push origin gh-pages
-git checkout master
+git commit -m "chore(deps): update schemas"
